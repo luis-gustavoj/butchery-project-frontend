94c1a2ce0ab29d49991feecabf85f441e38c74ae
Luis Silva <luisg.juliao@gmail.com>
2022-03-11 16:12:57 -0300

    feat: added new lib and next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withReactSvg = require("next-react-svg");
+const path = require("path");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+const nextConfig = withReactSvg({
+  reactStrictMode: true,
+  include: path.resolve(__dirname, "src/assets/svg"),
+
+  webpack: (config) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (/css-loader[/\\](?:cjs|dist|src)/.test(moduleLoader.loader)) {
+          if (typeof moduleLoader.options.modules === "object") {
+            moduleLoader.options.modules = {
+              ...moduleLoader.options.modules,
+              exportLocalsConvention: "camelCaseOnly",
+            };
+          }
+        }
+      });
+    });
+
+    return config;
+  },
+});
+
+module.exports = nextConfig;
