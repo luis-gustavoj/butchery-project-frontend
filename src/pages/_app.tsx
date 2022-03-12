82038d8bbb0b207744dfba8859261e79fb19b87b
Luis Silva <luisg.juliao@gmail.com>
2022-03-12 17:36:53 -0300

    feat: added products context provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
+import Modal from "react-modal";
+
+// Context
+import { ProductsContextProvider } from "src/contexts/products/context";
+
+// Styles
 import "../styles/globals.scss";
 
+Modal.setAppElement("#__next");
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ProductsContextProvider>
+      <Component {...pageProps} />
+    </ProductsContextProvider>
+  );
 }
 
 export default MyApp;
