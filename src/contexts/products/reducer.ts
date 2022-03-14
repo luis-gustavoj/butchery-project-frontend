b1f71b9f1cf8d6b7d3e3ea9e57b0c1cc98758575
Luis Silva <luisg.juliao@gmail.com>
2022-03-14 16:56:10 -0300

    feat: added function to edit products

--- src/contexts/products/reducer.ts
+++ src/contexts/products/reducer.ts
@@ -1,5 +1,5 @@
 import { ProductType, ProductsType } from "src/@types";
-import { ADD_PRODUCT } from "./actions";
+import { ADD_PRODUCT, EDIT_PRODUCT } from "./actions";
 
 export type ActionType = {
   type: string;
@@ -27,8 +27,28 @@ export const productsReducer = (state = initialState, action: ActionType) => {
           {
             name: payload.product.name,
             type: payload.product.type,
+            id: String(Math.random().toString(16).slice(2)),
           },
         ],
+      } as ProductsType;
+    case EDIT_PRODUCT:
+      const newProductsCategoryArray = state[payload.product.category].map(
+        (product) => {
+          if (product.id === payload.product.id) {
+            return {
+              name: payload.product.name,
+              type: payload.product.type,
+              id: product.id,
+            };
+          } else {
+            return { ...product };
+          }
+        }
+      );
+
+      return {
+        ...state,
+        [payload.product.category]: newProductsCategoryArray,
       } as ProductsType;
   }
 };
