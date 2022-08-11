f35030f1281e5e001acfc913736d44df88fbc22c
Luis Silva <luisg.juliao@gmail.com>
2022-08-11 14:48:50 -0300

    feat: results analysis module

---
+++ src/modules/ResultsAnalysis/ResultsAnalysis.tsx
@@ -0,0 +1,29 @@
+import { OperationAnalysis } from "src/components/results-operational-analysis";
+import { Tabs } from "src/components/tabs";
+import { TabContainer } from "src/components/tabs/tab-container";
+import { Layout } from "../layout";
+import styles from "./styles.module.scss";
+
+export const ResultsAnalysisModule = () => {
+  return (
+    <Layout>
+      <div className={styles.titleContainer}>
+        <h1>Análise de rendimento</h1>
+      </div>
+      <Tabs containerClassName={styles.tabsContainer}>
+        <TabContainer label="Bovino" className={styles.tabContainer}>
+          <OperationAnalysis />
+        </TabContainer>
+        <TabContainer label="Suíno" className={styles.tabContainer}>
+          <OperationAnalysis />
+        </TabContainer>
+        <TabContainer label="Aves" className={styles.tabContainer}>
+          <OperationAnalysis />
+        </TabContainer>
+        <TabContainer label="Caixaria" className={styles.tabContainer}>
+          <OperationAnalysis />
+        </TabContainer>
+      </Tabs>
+    </Layout>
+  );
+};
