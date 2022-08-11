470c0c18f2d6c99cef2a73ae1381a35a244d3f07
Luis Silva <luisg.juliao@gmail.com>
2022-08-11 14:46:34 -0300

    feat: login form

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,19 +1,44 @@
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { useAuthContext } from "src/contexts/AuthContext";
 import { Button } from "../button";
 import { Input } from "../input/text-input";
 import styles from "./styles.module.scss";
 
+type LoginFormInputs = {
+  email: string;
+  password: string;
+};
+
 export const LoginForm = () => {
+  const { handleSubmit, register } = useForm<LoginFormInputs>();
+  const { signIn } = useAuthContext();
+
+  const onSubmit = async (formData: LoginFormInputs) => {
+    try {
+      await signIn(formData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <div className={styles.labeledInput}>
         <label>E-mail</label>
-        <Input type="email" />
+        <Input type="email" {...register("email")} />
       </div>
       <div className={styles.labeledInput}>
         <label>Senha</label>
-        <Input type="password" />
+        <Input type="password" {...register("password")} />
+      </div>
+      <div className={styles.additionalInfo}>
+        <Link href="/forgot-password">Esqueceu sua senha?</Link>
       </div>
       <Button type="submit">Login</Button>
+      <div className={styles.additionalInfo}>
+        <Link href="/register">Quero criar uma conta</Link>
+      </div>
     </form>
   );
 };
